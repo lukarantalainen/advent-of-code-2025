00908fb051c7e3bcb1d47dcb34a9625f1b57decf
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2025-12-15 20:55:49 +0200

    day 3 part 1 solution

--- day03.cpp
+++ day03.cpp
@@ -5,17 +5,35 @@
 void count_joltage(std::string f) {
   std::fstream input(f);
   std::string line;
+  int joltage{0};
   while (std::getline(input, line)) {
-    int first{line[0]};
     int highest{0};
+    int second_highest{0};
+    int list_index{0};
+
     for (auto c : line) {
-      if (c > highest) highest = c;
-      first = c;
+      if (c > highest && list_index < line.length() - 1) {
+        highest = c;
+        second_highest = 0;
+      }
+
+      else if (second_highest < c) {
+        second_highest = c;
+      }
+
+      list_index++;
     }
+
+    std::string jolt_str =
+        std::to_string(highest -= '0') + std::to_string(second_highest -= '0');
+
+    joltage += std::stoi(jolt_str);
   }
+
+  std::cout << joltage;
 }
 
 int main() {
-  count_joltage("tests\\input03-test.txt");
+  count_joltage("inputs\\input03.txt");
   return 0;
 }
---
+++ day03p2.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void count_joltage(std::string f) {
+  std::fstream input(f);
+  std::string line;
+
+  std::getline(input, line);
+
+  char iterate_bank(std::string bank, int last_highest, int char_index) {
+    int char_index{0};
+  }
+}
+
+int main() { return 0; }
