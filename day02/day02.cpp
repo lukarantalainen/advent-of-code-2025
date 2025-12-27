d9cfb0bb03bfacbd156a509dd7eaaab9a6b07a29
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2025-12-27 21:48:50 +0200

    d2p2 solution

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -1,72 +1,58 @@
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
+bool check_repeating(long long num);
+
 std::vector<std::string> parse_string(std::string f) {
   std::fstream input(f);
   std::string line;
   std::getline(input, line);
-
   std::vector<std::string> ranges;
 
-  int prev_delim{};
-  int length{};
-  int char_index{};
+  int prev_delim{0};
+  int length{0};
+  int char_index{0};
   std::string range;
 
-  for (auto c : line) {
+  for (int i = 0; i <= line.length(); i++) {
     char_index++;
-    if (c == ',' || char_index == line.length() - 1) {
-      if (char_index == line.length() - 1) {
-        length += 2;
-      }
-
-      else if (prev_delim == 0) {
-        length++;
-      }
-
-      range = line.substr(prev_delim, length - 1);
+    if (line[i] == ',' || i == line.length()) {
+      range = line.substr(prev_delim, length);
       ranges.push_back(range);
       prev_delim = char_index;
       length = 0;
+    } else {
+      length++;
     }
-
-    length++;
   }
-
   return ranges;
 }
 
 std::vector<std::pair<std::string, std::string>> pair_values(
     std::vector<std::string> input) {
   std::vector<std::pair<std::string, std::string>> range_pairs;
-  for (auto s : input) {
-    int pair_num{};
-    int char_index = 0, length = 0;
-    std::string first, last;
-
-    for (auto c : s) {
+  for (std::string s : input) {
+    int char_index{0};
+    for (char c : s) {
       char_index++;
       if (c == '-') {
-        first = s.substr(0, char_index);
-        last = s.substr(char_index, (s.length() - char_index));
+        std::string first{s.substr(0, char_index)};
+        std::string last{s.substr(char_index)};
         range_pairs.push_back(std::make_pair(first, last));
       }
-      length++;
     }
-    pair_num++;
   }
-
   return range_pairs;
 }
 
-void part_one(std::vector<std::pair<std::string, std::string>> input) {
-  long long total{};
+long long part_one(std::vector<std::pair<std::string, std::string>> input) {
+  long long total{0};
   for (auto p : input) {
     auto first = std::stoll(p.first);
     auto second = std::stoll(p.second);
-
     for (long long i = first; i <= second; i++) {
       auto id = std::to_string(i);
       if (id.length() % 2 == 0) {
@@ -81,50 +67,47 @@ void part_one(std::vector<std::pair<std::string, std::string>> input) {
       }
     }
   }
-  std::cout << total << "\n";
+  return total;
 }
 
-void part_two(std::vector<std::pair<std::string, std::string>> input) {
+bool check_repeating(long long num) {
+  std::string id = std::to_string(num);
+  bool repeating = false;
+  const int id_len = id.length();
+
+  for (int i = 1; i <= id_len / 2; i++) {
+    if (id_len % i != 0) continue;
+    auto pattern = id.substr(0, i);
+    const int pat_len = pattern.length();
+    std::string counterpart{};
+    for (int j = 0; j < id_len / i; j++) {
+      counterpart.append(pattern);
+    }
+    if (id == counterpart) {
+      return true;
+    }
+  }
+  return repeating;
+}
+
+long long part_two(std::vector<std::pair<std::string, std::string>> input) {
+  long long total{0};
   for (auto p : input) {
-    int first{std::stoll(p.first)};
-    int second{std::stoll(p.second)};
-
-    for (long long i = first; i > second; i++) {
-      int first_char{0};
-      int length{0};
-      int pattern_length{0};
-      std::vector<char> pattern{};
-      int str_len{std::to_string(i).length()};
-      bool false_id = false;
-      for (char c : std::to_string(i)) {
-        if (length == 0) {
-          first_char = c;
-        } else if (c != first_char) {
-          pattern.push_back(c);
-          pattern_length = c;
-
-          for (int j = str_len; j < str_len / pattern.size(); j++) {
-            std::string str = (pattern.begin(), pattern.end());
-            if (str == std::to_string(i).substr(length, pattern_length)) {
-              false_id = true;
-            } else {
-              false_id = false;
-            }
-          }
-        } else if (c == first_char) {
-          pattern.push_back(first_char);
-        }
-        length++;
+    long long first{std::stoll(p.first)};
+    long long second{std::stoll(p.second)};
+    for (long long i = first; i <= second; i++) {
+      if (check_repeating(i) == true) {
+        total += i;
       }
     }
-    int length{0};
   }
+  return total;
 }
 
 int main() {
-  auto data = parse_string("../inputs/input02.txt");
-  part_one(pair_values(data));
-  part_two(pair_values(data));
+  auto data = pair_values(parse_string("../inputs/input02.txt"));
+  std::cout << part_one(data) << "\n";
+  std::cout << part_two(data) << "\n";
 
   return 0;
 }
