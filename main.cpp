ce746625d0fe222ba80fa25832c1660b57ef3be7
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-04-19 16:46:58 +0300

    solution for substring problem

--- main.cpp
+++ main.cpp
@@ -1,52 +1,56 @@
-#include <iostream>
-#include <map>
-#include "solutions.h"
 #include <cassert>
+#include <iostream>
+#include <vector>
 #include <string>
-#include <set>
 #include <unordered_set>
 
+#include "solutions.h"
+
 class Solution {
-public:
-    int lengthOfLongestSubstring(std::string s) {
-      if (s.length() == 0) {
-        return 0;
-      }
+ public:
+  int lengthOfLongestSubstring(std::string s) {
+    int longest{};
+    int left{};
+    int right{};
+
+    if (s.length() == 0) return 0;
+
+    std::unordered_set<char> seen{};
+    for (int i{0}; i < s.size(); ++i) {
       
-      int longest{1};
-      int left{};
-      int right{};
-      
-      std::unordered_set<char> seen{};
-      for (int i{0}; i<s.length(); ++i) {
-        if (!seen.count(s[i])) {
-          ++right;
-        } else {
-          longest = right-left+1;
-          std::cout << s.substr(left, right-left+1) << "\n";
-          seen.clear();
-          for (int j{left}; j<s.length(); ++j) {
-            if (s[j]==s[i]) {
-              left = j+1;
-              break;
-            }
+      if (seen.count(s[right])) {
+        if (right - left >= longest) {
+          longest = right - left;
+        }
+        
+        for (int j{left}; j < right; ++j) {
+          seen.erase(s[j]);
+          if (s[right] == s[j]) {
+            left = j+1;
+            break;
           }
         }
-        seen.insert(s[i]);
       }
-      return longest;
+      seen.insert(s[right]);
+      ++right;
     }
+    
+    if (right - left >= longest) {
+      longest = right - left;
+    }
+
+    return longest;
+  }
 };
 
 int main() {
-  //std::map<std::string, std::string*> funcs{};
+  // std::map<std::string, std::string*> funcs{};
 
   Solution s;
-  std::string str{"ababb"};
-  
-  s.lengthOfLongestSubstring("abcbadbd");
-  s.lengthOfLongestSubstring(str);
-  s.lengthOfLongestSubstring("ohomm");
+
+  std::cout << s.lengthOfLongestSubstring("") << "\n";
+  std::cout << s.lengthOfLongestSubstring("au") << "\n";
+  std::cout << s.lengthOfLongestSubstring("babcdbaba") << "\n";
   std::cout << "\n";
 
   return 0;
