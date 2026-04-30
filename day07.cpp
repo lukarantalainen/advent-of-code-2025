bea2d20dcc03ca87f25cd26c027087755b4858e6
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-04-30 17:19:30 +0300

    day7 p1 solution

---
+++ day07.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+namespace Day07 {
+const std::string FILENAME{"inputs/input07.txt"};
+long long part_one() {
+    std::ifstream input(FILENAME);
+    std::string line;
+
+    long long beams{};
+    std::unordered_set<int> previous_line{};
+    std::unordered_set<int> current_line{};
+
+    std::vector<std::string> tree{};
+    std::string prev{};
+    std::string curr{};
+
+    while (std::getline(input, line)) {
+        curr = line;
+
+        for (int i{0}; i<line.length(); ++i) {
+            if (line[i] == 'S') {
+                current_line.insert(i);
+                curr[i] = 'S';
+                break;
+            } else if (line[i] == '^' && previous_line.count(i)) {
+                current_line.erase(i);
+                ++beams;
+                if (!current_line.count(i-1)) {
+                    curr[i-1] = '|';
+                    current_line.insert(i-1);                    
+                }
+                if (!current_line.count(i+1)) {
+                    curr[i+1] = '|';
+                    current_line.insert(i+1);
+                }
+            } else if (previous_line.count(i)) {
+                curr[i] = '|';
+                current_line.insert(i);
+            } 
+        }
+        tree.push_back(std::string(curr.begin(), curr.end()));
+        previous_line = current_line;
+        prev = curr;
+    }
+    input.close();
+    
+    for (auto l : tree) {
+        std::cout << l << "\n";
+    }
+
+    return beams;
+}
+
+}
+
+int main()  {
+    std::cout << Day07::part_one() << "\n"; 
+
+    return 0;
+}
+    
